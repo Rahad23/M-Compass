3cf7882fe11fb5471ad88d4ee1f5caa93b99bfa2
Rahad23 <rahadhasan33675@gmail.com>
2023-07-27 16:48:59 +0600

    add swiper js

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -4,23 +4,33 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Navigation, Pagination, Mousewheel, Keyboard, Autoplay } from 'swiper/modules';
+import "./SliderStyle/SliderStyle.css";
 import slideImg from "../../assets/sliderImg/slide1.jpg";
 const Slider = () => {
     return (
         <div>
             <Swiper
+            style={{
+                "--swiper-navigation-size": "25px",
+                "--swiper-pagination-bullet-width": "15px",
+                "--swiper-pagination-bullet-height": "15px","--swiper-pagination-bullet-inactive-opacity": ".6",
+              }}
                 slidesPerView={1}
                 spaceBetween={30}
                 loop={true}
                 pagination={{
                     clickable: true,
                 }}
+                avigation={{
+                    prevEl: '.swiper-button-prev',
+                    nextEl: '.swiper-button-next',
+                }}
                 autoplay={{
                     delay: 3000,
-                    disableOnInteraction: false, // Set to false if you want the autoplay to continue even when the user interacts with the slider
+                    disableOnInteraction: false,
                 }}
                 navigation={true}
-                modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]} // Include Autoplay module here
+                modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
                 className="mySwiper"
             >
                 <SwiperSlide><img src={slideImg} alt="" /></SwiperSlide>
